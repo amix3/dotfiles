7b5f391946fe12dcfed4516bbc8c99aba9050f50
amix3 <112207774+amix3@users.noreply.github.com>
2023-04-15 14:04:58 +0330

    mpv -- added osc

--- .config/mpv/scripts/pause-indicator.lua
+++ .config/mpv/scripts/pause-indicator.lua
@@ -13,7 +13,7 @@ local log = {
     local level = 2
     if "table" ~= type(item) then
       msg.info(tostring(item))
-      return 
+      return
     end
     local count = 1
     local tablecount = 1
@@ -86,7 +86,7 @@ local settingsMeta = {
       local dirExists = mp.find_config_file(configDir)
       if dirExists and not utils.readdir(configDir) then
         log.warn(('Configuration migration failed. %q exists and does not appear to be a folder'):format(configDir))
-        return 
+        return
       else
         if not dirExists then
           local res = utils.subprocess({
@@ -97,7 +97,7 @@ local settingsMeta = {
           })
           if res.error or res.status ~= 0 then
             log.warn(('Making directory %q failed.'):format(configDir))
-            return 
+            return
           end
         end
       end
@@ -110,7 +110,7 @@ local settingsMeta = {
       })
       if res.error or res.status ~= 0 then
         log.warn(('Moving file %q -> %q failed.'):format(oldConfig, newConfig))
-        return 
+        return
       end
       if mp.find_config_file(newConfigFile) then
         return log.info('Configuration successfully migrated.')
@@ -140,12 +140,11 @@ helpText['display-scale-factor'] = [[Acts as a multiplier to increase the size o
 DPI displays that cause the UI to be rendered too small (happens at least on
 macOS).
 ]]
-settings['default-style'] = [[\fnSource Sans Pro\b1\bord2\shad0\fs30\c&HE3E3E3&\3c&H2D2D2D&]]
+settings['default-style'] = [[\fnSource Sans Pro\b1\bord2\shad0\fs30\c&HFC799E&\3c&H2D2D2D&]]
 helpText['default-style'] = [[Default style that is applied to all UI elements. A string of ASS override tags.
 Individual elements have their own style settings which override the tags here.
 Changing the font will likely require changing the hover-time margin settings
 and the offscreen-pos settings.
-
 Here are some useful ASS override tags (omit square brackets):
 \fn[Font Name]: sets the font to the named font.
 \fs[number]: sets the font size to the given number.
@@ -197,7 +196,6 @@ settings['seek-precision'] = 'exact'
 helpText['seek-precision'] = [[Affects precision of seeks due to clicks on the progress bar. Should be 'exact' or
 'keyframes'. Exact is slightly slower, but won't jump around between two
 different times when clicking in the same place.
-
 Actually, this gets passed directly into the `seek` command, so the value can be
 any of the arguments supported by mpv, though the ones above are the only ones
 that really make sense.
@@ -311,11 +309,11 @@ settings['pause-indicator'] = true
 helpText['pause-indicator'] = [[Sets whether or not the pause indicator is displayed. The pause indicator is a
 momentary icon that flashes in the middle of the screen, similar to youtube.
 ]]
-settings['pause-indicator-foreground-style'] = [[\c&HE3E3E3&]]
+settings['pause-indicator-foreground-style'] = [[\c&FBFCFC&]]
 helpText['pause-indicator-foreground-style'] = [[A string of ASS override tags that get applied only to the foreground of the
 pause indicator.
 ]]
-settings['pause-indicator-background-style'] = [[\c&H222222&]]
+settings['pause-indicator-background-style'] = [[\c&H2D2D2D&]]
 helpText['pause-indicator-background-style'] = [[A string of ASS override tags that get applied only to the background of the
 pause indicator.
 ]]
@@ -337,7 +335,7 @@ as a multiplier on the height of the active progress bar. A value greater than 1
 will cause the markers to be taller than the expanded progress bar, whereas a
 value less than 1 will cause them to be shorter.
 ]]
-settings['chapter-marker-before-style'] = [[\c&HE3E3E3&]]
+settings['chapter-marker-before-style'] = [[\c&HFC799E&]]
 helpText['chapter-marker-before-style'] = [[A string of ASS override tags that get applied only to chapter markers that have
 not yet been passed.
 ]]
@@ -356,7 +354,7 @@ significantly affect the smoothness of animations, and it is subject to the
 accuracy limits imposed by the scheduler mpv uses. Probably not worth changing
 unless you have major performance problems.
 ]]
-settings['animation-duration'] = 0.333
+settings['animation-duration'] = 0.25
 helpText['animation-duration'] = [[Controls how long the UI animations take. A value of 0 disables all animations
 (which breaks the pause indicator).
 ]]
@@ -430,7 +428,7 @@ do
     end,
     removeSortedList = function(self, elementList)
       if #elementList < 1 then
-        return 
+        return
       end
       for i = 1, #elementList - 1 do
         local element = table.remove(elementList)
@@ -623,7 +621,7 @@ do
   end
   self.animate = function()
     if #animationList == 0 then
-      return 
+      return
     end
     local currentTime = mp.get_time()
     for _, animation in ipairs(animationList) do
@@ -656,13 +654,13 @@ do
     end,
     addZone = function(self, zone)
       if zone == nil then
-        return 
+        return
       end
       return self.activityZones:insert(zone)
     end,
     removeZone = function(self, zone)
       if zone == nil then
-        return 
+        return
       end
       return self.activityZones:remove(zone)
     end,
@@ -741,7 +739,7 @@ do
       local displayDuration = settings['request-display-duration']
       mp.add_key_binding("tab", "request-display", function(event)
         if event.event == "repeat" then
-          return 
+          return
         end
         if event.event == "down" or event.event == "press" then
           if displayRequestTimer then
